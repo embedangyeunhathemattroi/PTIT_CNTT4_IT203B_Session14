233745ac8454ae6b092d273d5aac6e5c9299dc87
helloPTIT <phamngoclinhtq2006@gmail.com>
2026-04-01 00:31:02 +0700

    Update DBConnection.java

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/cyber_gaming?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/cyber_gaming";
     private static final String USER = "root"; // username MySQL
     private static final String PASS = "123456"; // mật khẩu MySQL
 
