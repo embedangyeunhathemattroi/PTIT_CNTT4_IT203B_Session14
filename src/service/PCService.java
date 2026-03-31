84d5d0d20359a95023b9ec1c645bbe51eac8769c
helloPTIT <phamngoclinhtq2006@gmail.com>
2026-04-01 00:41:25 +0700

    Refactor PCService to use PCDAO for data operations

--- src/service/PCService.java
+++ src/service/PCService.java
@@ -4,61 +4,49 @@ import model.PC;
 import java.util.ArrayList;
 import java.util.List;
 
+import DAO.PCDAO;
+
 public class PCService {
 
-    private List<PC> pcs; // khai báo biến pcs
-
-    public PCService() {
-        pcs = new ArrayList<>();
-        pcs.add(new PC(1, "PC Asus", 1, "i7, RTX 2060, 16GB RAM", 15000, "AVAILABLE"));
-        pcs.add(new PC(2, "Mac", 1, "M1, 16GB", 200000, "AVAILABLE"));
-
-    }
-
+    private static PCDAO pcDAO = new PCDAO();
     // Lấy danh sách tất cả máy
     public List<PC> getAllPCs() {
-        return pcs;
+        return pcDAO.findAll();
+    }
+
+    public PC getPCById(int id) {
+        return pcDAO.findById(id);
     }
 
     // Lấy danh sách máy trống
     public List<PC> getAvailablePCs() {
-        List<PC> danhSach = new ArrayList<>();
-        for (PC pc : pcs) {
-            if ("AVAILABLE".equalsIgnoreCase(pc.getStatus())) {
-                danhSach.add(pc);
-            }
-        }
-        return danhSach;
+        return pcDAO.findAvailable();
     }
 
-    // Lấy giá theo ID
-    public double getPrice(int pcId) {
-        for (PC pc : pcs) {
-            if (pc.getId() == pcId) {
-                return pc.getPrice();
-            }
+    public boolean deletePC(int id) {
+        return pcDAO.delete(id);
+    }
+
+    public boolean updatePC(int id, String name, String config, double price) {
+        try {
+            return pcDAO.update(id, name, config, price);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
-        System.out.println(" Không tìm thấy PC ID " + pcId + ", mặc định giá = 0");
-        return 0;
     }
 
     // Cập nhật trạng thái máy
     public void updateStatus(int pcId, String status) {
-        for (PC pc : pcs) {
-            if (pc.getId() == pcId) {
-                pc.setStatus(status);
-                System.out.println("Máy " + pcId + " đã được cập nhật trạng thái: " + status);
-                return;
-            }
-        }
-        System.out.println("Không tìm thấy máy với ID: " + pcId);
+        // not implemented yet
     }
 
     // Thêm máy mới
     public boolean addPC(String name, int categoryId, String config, double price, String status) {
-        int newId = pcs.size() + 1; // tự tăng id
-        PC pc = new PC(newId, name, categoryId, config, price, status);
-        pcs.add(pc);
-        return true;
+        return pcDAO.insert(new PC(name, categoryId, config, price, status));
+    }
+
+    public boolean isPCNameExists(String name) {
+        return pcDAO.isPCNameExists(name);
     }
 }
