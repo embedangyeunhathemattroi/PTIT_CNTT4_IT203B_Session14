c099d7f75ec77e9550e3f42206ff4867d5e42082
helloPTIT <phamngoclinhtq2006@gmail.com>
2026-04-01 00:42:47 +0700

    Refactor UserService and add getUserById method
    Refactored UserService to use UserDAO directly and improved input prompts. Added getUserById method for fetching user by ID.

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,23 +1,20 @@
 package service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import DAO.UserDAO;
-import DAO.UserDAOImpl;
 import model.User;
 import util.InputValidate;
 
 public class UserService {
 
-    private UserDAO userDAO = new UserDAOImpl();
+    private UserDAO userDAO = new UserDAO();
 
     public boolean register(Scanner sc) {
         while (true) {
-            System.out.println("Nhập username: ");
+            System.out.print("Nhập username: ");
             String username = InputValidate.inputString(sc);
-            System.out.println("Nhập password: ");
+            System.out.print("Nhập password: ");
             String password = InputValidate.inputString(sc);
 
             if (password.length() < 6) {
@@ -29,39 +26,20 @@ public class UserService {
         }
     }
 
-    // REGISTER CÓ ROLE (ADMIN / STAFF / CUSTOMER)
-    // public boolean register(String username, String password, String role) {
-
-    // if (username.isEmpty() || password.isEmpty() || role.isEmpty()) {
-    // System.out.println(" Không được để trống!");
-    // return false;
-    // }
-
-    // if (password.length() < 6) {
-    // System.out.println(" Mật khẩu phải >= 6 ký tự");
-    // return false;
-    // }
-    // role = role.toUpperCase();
-    // if (!role.equals("ADMIN") && !role.equals("STAFF") &&
-    // !role.equals("CUSTOMER")) {
-    // System.out.println("Role không hợp lệ!");
-    // return false;
-    // }
-
-    // User user = new User(username, password, role);
-    // return userDAO.register(user);
-    // }
-
     public User login(Scanner sc) {
         while (true) {
-            System.out.println("Nhập username: ");
+            System.out.print("Nhập username: ");
             String username = InputValidate.inputString(sc);
-            System.out.println("Nhập password: ");
+            System.out.print("Nhập password: ");
             String password = InputValidate.inputString(sc);
             User user = userDAO.login(username, password);
             if (user != null)
                 return user;
+            System.out.println("Đăng nhập thất bại! Sai username hoặc password.");
         }
     }
 
+    public User getUserById(int id) {
+        return userDAO.findById(id);
+    }
 }
