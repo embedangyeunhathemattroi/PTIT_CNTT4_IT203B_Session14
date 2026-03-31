2da00f20a505638949dd25613d3d03666e433ec1
helloPTIT <phamngoclinhtq2006@gmail.com>
2026-04-01 00:31:24 +0700

    Update InputValidate.java

--- src/util/InputValidate.java
+++ src/util/InputValidate.java
@@ -1,8 +1,12 @@
 package util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class InputValidate {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     public static int inputInteger(Scanner scanner) {
         while (true) {
             try {
@@ -34,6 +38,30 @@ public class InputValidate {
                 return line;
             } catch (IllegalArgumentException e) {
                 System.out.println("Không được để trống.");
+            }
+        }
+    }
+
+    public static String inputString(Scanner scanner, boolean allowEmpty) {
+        while (true) {
+            try {
+                String line = scanner.nextLine();
+                if (!allowEmpty && (line == "" || line.trim().isEmpty()))
+                    throw new IllegalArgumentException();
+                return line;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Không được để trống.");
+            }
+        }
+    }
+
+    public static LocalDateTime inputDateTime(Scanner sc) {
+        while (true) {
+            try {
+                String input = sc.nextLine().trim();
+                return LocalDateTime.parse(input, formatter);
+            } catch (DateTimeParseException e) {
+                System.out.println(" Định dạng thời gian không đúng, vui lòng nhập lại (yyyy-MM-dd HH:mm)");
             }
         }
     }
